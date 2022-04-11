e19da19b383876ad8daa7a5b9a5ddfcc0167d177
Dusko Dobranic <dusko.dobranic@akvelon.com>
2022-04-11 13:37:42 +0200

    Fix yq version to 4.24.2

--- images/linux/scripts/installers/yq.sh
+++ images/linux/scripts/installers/yq.sh
@@ -6,7 +6,9 @@ source $HELPER_SCRIPTS/install.sh
 YQ_BINARY=yq_linux_amd64
 
 # As per https://github.com/mikefarah/yq#wget
-YQ_URL=$(get_github_package_download_url "mikefarah/yq" "endswith(\"$YQ_BINARY.tar.gz\")")
+# temporary fix for https://github.com/mikefarah/yq/issues/1173
+#YQ_URL=$(get_github_package_download_url "mikefarah/yq" "endswith(\"$YQ_BINARY.tar.gz\")")
+YQ_URL="https://github.com/mikefarah/yq/releases/download/v4.24.2/yq_linux_amd64.tar.gz"
 echo "Downloading latest yq from $YQ_URL"
 
 download_with_retries "$YQ_URL" "/tmp" "${YQ_BINARY}.tar.gz"
