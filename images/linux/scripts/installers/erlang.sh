79ba388fd9464b56c08c9b31ea4bbc7d76826bff
Dusko Dobranic <dusko.dobranic@akvelon.com>
2022-12-02 11:44:59 +0100

    Update install procedure for u22

--- images/linux/scripts/installers/erlang.sh
+++ images/linux/scripts/installers/erlang.sh
@@ -11,10 +11,18 @@ source_list=/etc/apt/sources.list.d/eslerlang.list
 source_key=/usr/share/keyrings/eslerlang.gpg
 
 # Install Erlang
-wget -q -O - https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | gpg --dearmor > $source_key
-echo "deb [signed-by=$source_key]  https://packages.erlang-solutions.com/ubuntu $(lsb_release -cs) contrib" > $source_list
-apt-get update
-apt-get install -y --no-install-recommends esl-erlang
+if isUbuntu22; then
+    #echo "deb [signed-by=/usr/share/keyrings/eslerlang.gpg] https://packages.erlang-solutions.com/ubuntu focal contrib" | tee $source_list
+    #wget -qO- https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | gpg --dearmor | tee $source_key
+    wget -qO- https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | gpg --dearmor -o /etc/apt/trusted.gpg.d/erlang.gpg
+    apt-get update
+    apt-get install -y erlang
+else
+    wget -q -O - https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | gpg --dearmor > $source_key
+    echo "deb [signed-by=$source_key]  https://packages.erlang-solutions.com/ubuntu $(lsb_release -cs) contrib" > $source_list
+    apt-get update
+    apt-get install -y --no-install-recommends esl-erlang
+fi
 
 # Install rebar3
 rebar3_url="https://github.com/erlang/rebar3/releases/latest/download/rebar3"
